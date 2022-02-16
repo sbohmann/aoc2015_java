f1627d013bf80d18a690b11172ffa6c55058f6f0
sbohmann <mail@sebastian-bohmann.com>
2022-02-16 22:01:40 +0100

    Sequence default implementations for map and flatmap

--- src/mini/Sequence.java
+++ src/mini/Sequence.java
@@ -1,5 +1,8 @@
 package mini;
 
+import mini.lazy.FlatMappedSequence;
+import mini.lazy.MappedSequence;
+
 import java.util.Iterator;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
@@ -8,9 +11,13 @@ import java.util.function.Function;
 public interface Sequence<T> extends Iterable<T> {
     void foreach(Consumer<? super T> action);
 
-    <U> Sequence<U> map(Function<? super T, ? extends U> transformation);
+    default <U> Sequence<U> map(Function<? super T, ? extends U> transformation) {
+        return new MappedSequence<>(this, transformation);
+    }
 
-    <U> Sequence<U> flatmap(Function<? super T, Sequence<? extends U>> transformation);
+    default <U> Sequence<U> flatmap(Function<? super T, Sequence<? extends U>> transformation) {
+        return new FlatMappedSequence<>(this, transformation);
+    }
 
     default <U> U reduce(U initialValue, BiFunction<U, ? super T, ? extends U> processing) {
         throw new UnsupportedOperationException("Attempt to call reduce() on a non-iterable Sequence");
